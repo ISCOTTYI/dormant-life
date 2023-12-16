936de7d35c4a6905876044e080942f826549dbb4
Daniel Nevermann <nevermann@users.itp.uni-frankfurt.de>
2023-12-16 11:23:25 +0100

    Parallelize extinction time computation

--- extinction_time.py
+++ extinction_time.py
@@ -1,7 +1,6 @@
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
-plt.style.use("science")
+import multiprocessing
 from gol import CellularAutomaton, DormantLife
 
 
@@ -51,16 +50,26 @@ def dormant_life_extinction_times(grid_size: int,
 
 
 if __name__ == "__main__":
-    alphas = np.round(np.linspace(0, 0.4, 30), 3)
+    alphas = np.round(np.linspace(0, 1, 75), 3)
     np.savetxt("./data/dormant-life/extinction-time/alpha-range.dat",
                (alphas), header="Alpha values for which data is stored.")
-    for alpha in alphas:
+
+    def compute(alpha):
         print(f"alpha = {alpha}")
         fname = f"alpha-p{str(alpha)[2:]}.dat"
         parameters = (grid_size, q, alpha, t_max, runs) = (
-            30, 0.3701, alpha, 100_000, 5000
+            30, 0.3701, alpha, 1_000_000, 1000
         )
         data = dormant_life_extinction_times(*parameters, progress_updates=True)
         np.savetxt(f"./data/dormant-life/extinction-time/{fname}",
                    (data),
                    header=f"(grid_size, q, alpha, t_max, runs) = {str(parameters)}")
+
+
+    # Use the with statement to create and close the pool
+    with multiprocessing.Pool() as pool:
+        # Use imap_unordered to apply the function to each value of alpha in parallel
+        # and yield the results as they are ready, regardless of the order
+        for _ in pool.imap_unordered(compute, alphas):
+            pass
+
