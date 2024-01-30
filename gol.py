acbecd9ed6eac291f69e0f8c66c1a48617c21625
Daniel Henrik Nevermann <74931903+ISCOTTYI@users.noreply.github.com>
2024-01-30 11:38:21 +0100

    New Markovian model

--- gol.py
+++ gol.py
@@ -4,14 +4,15 @@ from scipy.ndimage import convolve
 
 DEAD = 0
 ALIVE = 1
-DORM = 2
+SPORE = 2
 
 
 class CellularAutomaton():
     """
     Base class for game of life models.
     """
-    def __init__(self, init_grid: np.ndarray, states: np.array, seed: int):
+    def __init__(self, init_grid: np.ndarray, states: np.array, seed: int,
+                 periodic_boundary: bool):
         # Ensure that init_grid is quadratic and only filled with states
         assert (len(init_grid.shape) == 2
                 and init_grid.shape[0] == init_grid.shape[1])
@@ -27,7 +28,8 @@ class CellularAutomaton():
             [1, 0, 1],
             [1, 1, 1],
         ])
-        if seed:
+        self.periodic_boundary = periodic_boundary
+        if seed is not None:
             self.rng = np.random.default_rng(seed)
         else:
             self.rng = np.random.default_rng()
@@ -35,15 +37,16 @@ class CellularAutomaton():
     def count_state(self, state: int):
         return np.count_nonzero(self.grid == state)
     
-    def compute_neighbor_counts_for(self, state: int):
+    def compute_neighbor_counts_for(self, state: int, periodic_boundary=True):
         filtered_grid = (self.grid == state).astype(np.intc)
-        c = convolve(filtered_grid, self.conv_ker, mode="wrap")
+        mode = "wrap" if periodic_boundary else "constant"
+        c = convolve(filtered_grid, self.conv_ker, mode=mode, cval=0)
         return c
     
     def reinit_grid(self):
         raise NotImplementedError("Instance of CellularAutomaton may not be initialized!")
     
-    def step(self) -> np.ndarray:
+    def step(self, silent: bool = False) -> np.ndarray:
         raise NotImplementedError("Instance of CellularAutomaton does not implement rules!")
     
     def step_until(self, t: int) -> np.ndarray:
@@ -67,10 +70,11 @@ class CellularAutomaton():
             
 
 class GameOfLife(CellularAutomaton):
-    def __init__(self, init_grid: np.ndarray, seed: int = None):
+    def __init__(self, init_grid: np.ndarray, seed: int = None,
+                 periodic_boundary: bool = True):
         # 0: dead, 1: alive
         self.states = np.array([DEAD, ALIVE])
-        super().__init__(init_grid, self.states, seed)
+        super().__init__(init_grid, self.states, seed, periodic_boundary)
         self.alive_neighbor_counts = None
     
     @property
@@ -86,26 +90,37 @@ class GameOfLife(CellularAutomaton):
         dims = (self.N, self.N)
         self.grid = self.rng.choice(self.states, p=(p_dead, p_alive), size=dims)
     
-    def step(self) -> np.ndarray:
+    def step(self, silent: bool = False) -> np.ndarray:
+        """
+        Perform a step in Game of Life.
+        A silent step is only computed and returned but does not count as a time
+        step and is not stored.
+        """
         ngrid = self.grid.copy()
         # Create array with 8-neighbor sums by convolution, using periodic
         # boundary conditions.
-        self.alive_neighbor_counts = c = self.compute_neighbor_counts_for(ALIVE)
+        self.alive_neighbor_counts = c = self.compute_neighbor_counts_for(
+            ALIVE, periodic_boundary=self.periodic_boundary)
         # Apply rules of game of life
         ngrid[(self.grid == ALIVE) & ((c < 2) | (c > 3))] = DEAD
         ngrid[(self.grid == DEAD) & (c == 3)] = ALIVE
-        self.grid = ngrid
-        self.t += 1
+        # Update grid and time
+        if not silent:
+            self.grid = ngrid
+            self.t += 1
         return ngrid
     
 
-class DormantLife(CellularAutomaton):
-    def __init__(self, init_grid:np.ndarray, alpha: float = 1, seed: int=None):
-        # 0: dead, 1: alive, 2: dormant
-        self.states = np.array([DEAD, ALIVE, DORM])
-        super().__init__(init_grid, self.states, seed)
-        
-        self.p_grid = np.ones((self.N, self.N))
+class SporeLife(CellularAutomaton):
+    def __init__(self, init_grid: np.ndarray, alpha: float = 1,
+                 seed: int = None, periodic_boundary: bool = True):
+        """
+        For alpha = 1 we get deterministic SporeLife, for alpha = 0 we get Game
+        of Life.
+        """
+        # 0: dead, 1: alive, 2: spore
+        self.states = np.array([DEAD, ALIVE, SPORE])
+        super().__init__(init_grid, self.states, seed, periodic_boundary)
         
         self.alive_neighbor_counts = None
 
@@ -117,14 +132,14 @@ class DormantLife(CellularAutomaton):
         return self.count_state(ALIVE)
     
     @property
-    def dorm_count(self):
-        return self.count_state(DORM)
+    def spore_count(self):
+        return self.count_state(SPORE)
     
     def alive_count_time_series(self, t_max: int) -> np.ndarray:
         return self.state_count_time_series(t_max, ALIVE)
     
-    def dorm_count_time_series(self, t_max: int) -> np.ndarray:
-        return self.state_count_time_series(t_max, DORM)
+    def spore_count_time_series(self, t_max: int) -> np.ndarray:
+        return self.state_count_time_series(t_max, SPORE)
     
     def reinit_grid(self, p_alive: float, p_dorm: float):
         assert 0 <= p_alive <= 1 and 0 <= p_dorm <= 1 and p_alive + p_dorm < 1
@@ -132,39 +147,51 @@ class DormantLife(CellularAutomaton):
         dims = (self.N, self.N)
         prob = (p_dead, p_alive, p_dorm)
         self.grid = self.rng.choice(self.states, p=prob, size=dims)
-
-    def step(self) -> np.ndarray:
+    
+    def deterministic_step(self, silent: bool = False) -> np.ndarray:
         """
-        Perform a step in DormantLife. For alpha = 1 we get deterministic
-        DormantLife, for alpha = 0 we get Game of Life.
+        Perform a step in SporeLife without stochasticity, i.e. ignore the given
+        alpha and pretend that alpha = 1.
+        A silent step is only computed and returned but does not count as a time
+        step and is not stored.
         """
-        self.p_grid *= self.alpha # decay
         ngrid = self.grid.copy()
         # Create array with 8-neighbor ALIVE counts by convolution, using
         # periodic boundary conditions.
-        self.alive_neighbor_counts = c = self.compute_neighbor_counts_for(ALIVE)
+        self.alive_neighbor_counts = c = self.compute_neighbor_counts_for(
+            ALIVE, periodic_boundary=self.periodic_boundary)
         # Apply rules of game of life w/ dormancy
         # DEAD awake
         ngrid[(self.grid == DEAD)
               & (c == 3)] = ALIVE
         # DORMANT awake
-        # Awakening with 2 ALIVE neighbors happens only with probability given
-        # by p_grid. If alpha = 1, p_grid is always 1 and we get non-stochastic
-        # DormantLife, if alpha = 0, p_grid is always 0 and we get GoL.
-        decision_grid = self.rng.random((self.N, self.N))
-        ngrid[(self.grid == DORM)
-              & ((c == 2) & (decision_grid < self.p_grid))] = ALIVE
-        ngrid[(self.grid == DORM)
-              & (c == 3)] = ALIVE
+        ngrid[(self.grid == SPORE)
+              & ((c == 2) | (c == 3))] = ALIVE
         # ALIVE dies
         ngrid[(self.grid == ALIVE)
               & ((c < 1) | (c > 3))] = DEAD
         # ALIVE goes DORMANT
         ngrid[(self.grid == ALIVE)
-              & (c == 1)] = DORM
-        # Decay probability reset
-        self.p_grid[(self.grid == ALIVE)
-                    & (c == 1)] = 1
-        self.grid = ngrid
-        self.t += 1
+              & (c == 1)] = SPORE
+        # Update grid and time
+        if not silent:
+            self.grid = ngrid
+            self.t += 1
+        return ngrid
+
+    def step(self, silent: bool = False) -> np.ndarray:
+        """
+        Perform a (possibly stochastic) step in SporeLife.
+        A silent step is only computed and returned but does not count as a time
+        step and is not stored.
+        """
+        ngrid = self.deterministic_step(silent=True)
+        # Randomly kill SPOREs in ngrid based on alpha
+        decision_grid = self.rng.random((self.N, self.N))
+        ngrid[(ngrid == SPORE)
+              & (decision_grid < (1-self.alpha))] = DEAD
+        # Update grid and time
+        if not silent:
+            self.grid = ngrid
+            self.t += 1
         return ngrid
